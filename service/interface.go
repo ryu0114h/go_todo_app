68c13596f7a54e95c74773489d9ac56c74314111
星野龍 <ryu0114h@gmail.com>
2023-04-02 23:49:47 +0900

    ユーザ登録機能の作成

--- service/interface.go
+++ service/interface.go
@@ -15,9 +15,9 @@ type TaskLister interface {
 	ListTasks(ctx context.Context, db store.Queryer /*, id entity.UserID */) (entity.Tasks, error)
 }
 
-// type UserRegister interface {
-// 	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
-// }
+type UserRegister interface {
+	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
+}
 
 // type UserGetter interface {
 // 	GetUser(ctx context.Context, db store.Queryer, name string) (*entity.User, error)
---
+++ service/register_user.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ryu0114h/go_todo_app/entity"
+	"github.com/ryu0114h/go_todo_app/store"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type RegisterUser struct {
+	DB   store.Execer
+	Repo UserRegister
+}
+
+func (r *RegisterUser) RegisterUser(
+	ctx context.Context, name, password, role string,
+) (*entity.User, error) {
+	pw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("cannot hash password: %w", err)
+	}
+	u := &entity.User{
+		Name:     name,
+		Password: string(pw),
+		Role:     role,
+	}
+	if err := r.Repo.RegisterUser(ctx, r.DB, u); err != nil {
+		return nil, fmt.Errorf("cannot register user: %w", err)
+	}
+	return u, nil
+}
