64aa5d112f8ec2b44f0f041f312f733ce0cc8136
星野龍 <ryu0114h@gmail.com>
2023-04-15 14:44:24 +0900

    jwt_test

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/ryu0114h/go_todo_app/clock"
 	"github.com/ryu0114h/go_todo_app/entity"
 	"github.com/ryu0114h/go_todo_app/store"
-	"github.com/ryu0114h/go_todo_app/testutil"
 	"github.com/ryu0114h/go_todo_app/testutil/fixture"
 )
 
@@ -26,74 +25,17 @@ func TestEmbed(t *testing.T) {
 	if !bytes.Contains(rawPubKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPubKey)
 	}
-	want = []byte("-----BEGIN PRIVATE KEY-----")
+	want = []byte("-----BEGIN RSA PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPrivKey)
 	}
 }
 
-// デバッグコード
-func TestJWTer(t *testing.T) {
-	store := &store.KVS{Cli: testutil.OpenRedisForTest(t)}
-	wantID := entity.UserID(20)
-	u := entity.User{
-		ID:       wantID,
-		Name:     "budougumi",
-		Password: "test",
-		Role:     "admin",
-		Created:  time.Time{},
-		Modified: time.Time{},
-	}
-	sut, err := NewJWTer(store, clock.RealClocker{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	signed, err := sut.GenerateToken(context.Background(), u)
-	if err != nil {
-		t.Fatalf("failed to generate jwt: %s", err)
-	}
-	req := httptest.NewRequest(
-		http.MethodGet,
-		`https://github.com/ryu0114h`,
-		nil)
-	req.Header.Set(`Authorization`, fmt.Sprintf(`Bearer %s`, signed))
-	t.Logf("generated\n%s\n", signed)
-
-	// HTTPハンドラーが受け取ったリクエストを想定
-	req, err = sut.FillContext(req)
-	if err != nil {
-		t.Fatalf("failed to initialize request: %v", err)
-	}
-	if !IsAdmin(req.Context()) {
-		t.Error("IsAdmin() should be true")
-	}
-	got, ok := GetUserID(req.Context())
-	if !ok {
-		t.Fatal("GetUserID() should be true")
-	}
-	if got != wantID {
-		t.Errorf("want %d, but got %d", wantID, got)
-	}
-}
-
-func TestNewJWTer(t *testing.T) {
-	got, err := NewJWTer(nil, nil)
-	if err != nil {
-		t.Fatalf("failed to initialze: %v", err)
-	}
-	if got.PublicKey == nil {
-		t.Error("invalid public key")
-	}
-	if got.PrivateKey == nil {
-		t.Error("invalid private key")
-	}
-}
-
 func TestJWTer_GenJWT(t *testing.T) {
 	ctx := context.Background()
+	moq := &StoreMock{}
 	wantID := entity.UserID(20)
 	u := fixture.User(&entity.User{ID: wantID})
-	moq := &StoreMock{}
 	moq.SaveFunc = func(ctx context.Context, key string, userID entity.UserID) error {
 		if userID != wantID {
 			t.Errorf("want %d, but got %d", wantID, userID)
@@ -113,7 +55,7 @@ func TestJWTer_GenJWT(t *testing.T) {
 	}
 }
 
-func TestJWTer_GetJWT(t *testing.T) {
+func TestJWTer_GetToken(t *testing.T) {
 	t.Parallel()
 
 	c := clock.FixedClocker{}
@@ -170,7 +112,7 @@ func (c FixedTomorrowClocker) Now() time.Time {
 	return clock.FixedClocker{}.Now().Add(24 * time.Hour)
 }
 
-func TestJWTer_GetJWT_NG(t *testing.T) {
+func TestJWTer_GetToken_NG(t *testing.T) {
 	t.Parallel()
 
 	c := clock.FixedClocker{}
