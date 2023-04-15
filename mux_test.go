28ec2a1b1372e7b7027ceedc4acdf0bcc94d471a
星野龍 <ryu0114h@gmail.com>
2023-04-15 14:34:05 +0900

    mux_test作成

---
+++ mux_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ryu0114h/go_todo_app/config"
+)
+
+func TestNewMux(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/health", nil)
+	ctx := context.Background()
+	cfg, err := config.New()
+	if err != nil {
+		t.Error("config.New() failed")
+	}
+	sut, cleanup, err := NewMux(ctx, cfg)
+	if err != nil {
+		t.Error("NewMux failed")
+	}
+	defer cleanup()
+	sut.ServeHTTP(w, r)
+	resp := w.Result()
+	t.Cleanup(func() { _ = resp.Body.Close() })
+	if resp.StatusCode != http.StatusOK {
+		t.Error("want status code 200, but", resp.StatusCode)
+	}
+	got, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+	want := `{"status": "ok"}`
+	if string(got) != want {
+		t.Errorf("want %q, but got %q", want, got)
+	}
+}
